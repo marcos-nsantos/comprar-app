2fc6b04e0cffcc0dcbf3ffd27530a47d857ca361
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2025-05-26 19:51:48 -0500

    feat: add Filter components to Home for task status display

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,6 +2,8 @@ import { Image, View } from "react-native";
 import { styles } from "@/app/Home/styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
 
 export function Home() {
   return (
@@ -13,7 +15,10 @@ export function Home() {
         <Button title="Entrar" />
       </View>
 
-      <View style={styles.content}></View>
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
