98d68d36f9b35314287c6bd852754a93478cd439
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2025-05-29 17:55:56 -0500

    feat: add description state management to Input component in Home

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -29,13 +29,17 @@ const ITEMS = [
 
 export function Home() {
   const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState("");
 
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
+        <Input
+          placeholder="O que você precisa comprar?"
+          onChangeText={setDescription}
+        />
         <Button title="Entrar" />
       </View>
 
